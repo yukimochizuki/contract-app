7e8ec1a64eb0f2e16c0526fd22659f69a923d403
yukimochizuki <ymochizuki@terrasky.co.jp>
2025-05-15 14:16:28 +0900

    Add Home and routing setup2

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
     return (
       <div>
         <h1>トップページ</h1>
-        <p>ようこそ！左のナビゲーションやURLから <code>/contracts</code> にアクセスしてみてください。</p>
+        <p>ようこそ！左のナビゲーションやURLから <Link to="/contracts">/contracts</Link> にアクセスしてみてください。</p>
       </div>
     );
 }
